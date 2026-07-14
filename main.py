f3e59404773a198045a7cbd7e62deccfb1bd07ea
morand <morandkentsa9@gmail.com>
2026-07-14 06:48:09 +0100

    mise à jour de la fonction de mise à jour du status d'une demande; creation d'une nouvelle route pour récupérer les photos; correction du calcul du premier jour de la semaine dans le calcul des statistiques

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ crons = Crons(app, state_backend=backend)
 # ── Crons ──
 current_date = date.today()
 
-@crons.cron(expr="9 11 * * *", name="init scoring")
+@crons.cron(expr="09 11 * * *", name="init scoring")
 def init_scoring():
     with get_db() as db:
         return init_pointage(db=db)
